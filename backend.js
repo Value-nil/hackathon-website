34c909f8a54582668f2961cfc2ec775264cf8a86
Value_nil <value.nil5@gmail.com>
2023-11-04 15:58:50 +0100

    Made connection with OpenAI api

--- backend.js
+++ backend.js
@@ -1,2 +1,18 @@
 const {OpenAI} = require("openai");
-const openai = new OpenAI();
+
+const openai = new OpenAI({
+  apiKey: ""
+});
+async function main(){
+    const completion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+            {
+                role: "user",
+                content: "Say this is an example"
+            }
+        ]
+    })
+    console.log(completion.choices[0].message.content);
+}
+main();
